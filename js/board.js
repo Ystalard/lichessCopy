57f432031dee652023ead2e32d52b91cb1c8a62f
Ystalard <nicolas.parisse.93@gmail.com>
2021-08-18 12:41:05 +0200

    commented drag functions and factorized functions

--- js/board.js
+++ js/board.js
@@ -1,3 +1,26 @@
 function switchBoardOrientation(board){
     board.classList.toggle('rotated')
 }
+
+function getPosition(event){
+    const board = event.path.find(element => element.classList.contains("board"))
+    const height = board.clientHeight
+    const width =  board.clientWidth
+    var offsetXFromBoard
+    var offsetYFromBoard
+
+    if (event.path[0].classList.contains("board")){
+        offsetXFromBoard = event.offsetX
+        offsetYFromBoard = event.offsetY
+    }
+    else {
+        offsetXFromBoard = event.offsetX + event.path[0].offsetLeft - board.offsetLeft
+        offsetYFromBoard = event.offsetY + event.path[0].offsetTop - board.offsetTop
+    }
+    var normalizedX = Math.trunc((offsetXFromBoard*8)/width)
+    var normalizedY = Math.trunc((offsetYFromBoard*8)/height)
+
+    console.log("selected position = " + String.fromCharCode((normalizedX + 1 + 64)).toLowerCase() + ((normalizedY - 7)*(-1) + 1))
+
+    return String.fromCharCode((normalizedX + 1 + 64)).toLowerCase() + ((normalizedY - 7)*(-1) + 1)
+}
--- js/selectSquare.js
+++ js/selectSquare.js
@@ -4,27 +4,11 @@ $(function(){
             e.preventDefault()
             console.log(e.button)
             const board = e.path.find(element => element.classList.contains("board"))
-            const height = board.clientHeight
-            const width =  board.clientWidth
-            var offsetXFromBoard
-            var offsetYFromBoard
 
-            if (e.path[0].classList.contains("board")){
-                offsetXFromBoard = e.offsetX
-                offsetYFromBoard = e.offsetY
-            }
-            else {
-                offsetXFromBoard = e.offsetX + e.path[0].offsetLeft - board.offsetLeft
-                offsetYFromBoard = e.offsetY + e.path[0].offsetTop - board.offsetTop
-            }
-            var normalizedX = Math.trunc((offsetXFromBoard*8)/width)
-            var normalizedY = Math.trunc((offsetYFromBoard*8)/height)
-
-            console.log("selected position = " + String.fromCharCode((normalizedX + 1 + 64)).toLowerCase() + ((normalizedY - 7)*(-1) + 1))
-            var position = String.fromCharCode((normalizedX + 1 + 64)).toLowerCase() + ((normalizedY - 7)*(-1) + 1)
             var coloredSquare
             var childNodesArray = Array.prototype.slice.call(board.childNodes)
-
+            var position = getPosition(e)
+            
             if (childNodesArray.find(element => element.localName == "square" && element.classList.contains(position))){
                 coloredSquare = childNodesArray.find(element => element.localName == "square" && element.classList.contains(position))
             } else {
