30afa661f7b920d8c4179f2092b05dcdda7fc452
Ystalard <nicolas.parisse.93@gmail.com>
2021-08-18 12:46:56 +0200

    Revert "commented drag functions and factorized functions"
    This reverts commit 57f432031dee652023ead2e32d52b91cb1c8a62f.

--- js/movePiece.js
+++ js/movePiece.js
@@ -1,36 +1,36 @@
 
 
-// function dragover_handler(ev) {
-//     ev.preventDefault();
+function dragover_handler(ev) {
+    ev.preventDefault();
     
-// }
+}
 
-// function dataToDrag(ev) {
-//     ev.dataTransfer.setData("draggedObject", ev.target.id)
-//     ev.target.style.cursor = 'default';
-//     var canvas = document.createElement('canvas');
-//     canvas.id     = "canvas";
-//     canvas.width  = 37;
-//     canvas.height = 37;
-//     let img = new Image();
-//     canvas.getContext('2d').drawImage(img,0,0);
+function dataToDrag(ev) {
+    ev.dataTransfer.setData("draggedObject", ev.target.id)
+    ev.target.style.cursor = 'default';
+    var canvas = document.createElement('canvas');
+    canvas.id     = "canvas";
+    canvas.width  = 37;
+    canvas.height = 37;
+    let img = new Image();
+    canvas.getContext('2d').drawImage(img,0,0);
     
-//     ev.target.classList.forEach(function(value){
-//         let stop = true;
-//         switch(value){
-//             case 'whitePawn':
-//                 img.src='./img/black_pawn.svg';
-//                 ev.dataTransfer.setDragImage(img,37/2,37/2);
-//                 stop = true
-//             break;
-//             default: 
-//             stop = false;
-//         }
-//         if(stop){
-//             return;
-//         }
-//     })
-// }
+    ev.target.classList.forEach(function(value){
+        let stop = true;
+        switch(value){
+            case 'whitePawn':
+                img.src='./img/black_pawn.svg';
+                ev.dataTransfer.setDragImage(img,37/2,37/2);
+                stop = true
+            break;
+            default: 
+            stop = false;
+        }
+        if(stop){
+            return;
+        }
+    })
+}
 
 function translatePiece(initPosition,finalPosition,timerange){
     const grid = document.querySelector(".board");
@@ -49,5 +49,5 @@ function translatePiece(initPosition,finalPosition,timerange){
 }
 
 $(function() {
-    translatePiece('a1','b7',100);    
+    translatePiece('a1','b7',600);    
 })
